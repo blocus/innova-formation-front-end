561fcc8a4b523d768a63f3d7363802e932d10767
meftah <meftah@meftah.lock>
2019-12-09 10:48:58 +0100

    correction exercie et ajout de la langue

---
+++ js/language.js
@@ -0,0 +1,10 @@
+var language = {
+    fr : {
+        helloMessageM : 'Bonjour M. ',
+        helloMessageF : 'Bonjour Mme. ',
+    },
+    en : {
+        helloMessageM : 'Hello Mr. ',
+        helloMessageF : 'Hello Ms. ',
+    },
+}
